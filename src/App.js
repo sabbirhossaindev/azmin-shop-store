1260974811dd0d82ac219d6f70c723fe2ffccc4c
sabbir hossain <mdsabbir477470@gmail.com>
2023-02-20 20:06:54 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import React from "react";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useThemeHook } from './GlobalComponents/ThemeProvider';
+import Header from './components/Header';
+// import { Router } from "@reach/router";
+
+// //Pages
+// import Home from './Pages/Home';
+// import Cart from './Pages/Cart';
+// import ProductDetails from "./Pages/ProductDetails";
+// import SignIn from "./Pages/SignIn";
+// import Register from "./Pages/Register";
+// import MyAccount from "./Pages/MyAccount";
+
 
 function App() {
+  const [theme] = useThemeHook();
   return (
-    <div className='mx-auto'>
-      <h1>Just set up done</h1>
-    </div>
+    <main className={theme? 'bg-black': 'bg-light-2'} style={{ height: '100vh', overflowY: 'auto'}}>
+      <Header/>
+      {/* <Router>
+        <Home path="/" />
+        <MyAccount path="my-account" />
+        <SignIn path="sign-in"/>
+        <Register path="register"/>
+        <ProductDetails path="product-details/:productId"/>
+        <Cart path="/cart" />
+      </Router> */}
+    </main>
   );
 }
 
